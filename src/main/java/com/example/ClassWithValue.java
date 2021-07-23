ad2f96d72f8001436f4273a832547207b6360182
Florian Braun <florian.braun@optum360.com>
2021-07-23 14:29:21 -0700

    Add tests for methods with arguments

--- src/main/java/com/example/ClassWithValue.java
+++ src/main/java/com/example/ClassWithValue.java
@@ -2,75 +2,24 @@ package com.example;
 
 public class ClassWithValue {
 
-	private int valueInteger;
-	private double valueDouble;
-	private float valueFloat;
-	private short valueShort;
-	private byte valueByte;
-	
-	public int getValueInteger() {
-		return valueInteger;
+	private String value;
+	private Double doubleValue;
+
+	public String getValue() {
+		return value;
 	}
-	public void setValueInteger(int valueInt) {
-		this.valueInteger = valueInt;
+
+	public void setValue(String value) {
+		this.value = value;
 	}
-	public double getValueDouble() {
-		return valueDouble;
+
+	public Double getDoubleValue() {
+		return doubleValue;
 	}
-	public void setValueDouble(double valueDouble) {
-		this.valueDouble = valueDouble;
-	}
-	public float getValueFloat() {
-		return valueFloat;
-	}
-	public void setValueFloat(float valueFloat) {
-		this.valueFloat = valueFloat;
-	}
-	public short getValueShort() {
-		return valueShort;
-	}
-	public void setValueShort(short valueShort) {
-		this.valueShort = valueShort;
-	}
-	public byte getValueByte() {
-		return valueByte;
-	}
-	public void setValueByte(byte valueByte) {
-		this.valueByte = valueByte;
+
+	public void setDoubleValue(double doubleValue) {
+		this.doubleValue = doubleValue;
 	}
 	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + valueByte;
-		long temp;
-		temp = Double.doubleToLongBits(valueDouble);
-		result = prime * result + (int) (temp ^ (temp >>> 32));
-		result = prime * result + Float.floatToIntBits(valueFloat);
-		result = prime * result + valueInteger;
-		result = prime * result + valueShort;
-		return result;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		ClassWithValue other = (ClassWithValue) obj;
-		if (valueByte != other.valueByte)
-			return false;
-		if (Double.doubleToLongBits(valueDouble) != Double.doubleToLongBits(other.valueDouble))
-			return false;
-		if (Float.floatToIntBits(valueFloat) != Float.floatToIntBits(other.valueFloat))
-			return false;
-		if (valueInteger != other.valueInteger)
-			return false;
-		if (valueShort != other.valueShort)
-			return false;
-		return true;
-	}
+	
 }
